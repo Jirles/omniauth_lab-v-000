095dfe7875390490407bd204e47e5ee3e4d3d8d2
Emily Jirles <emily.jirles@gmail.com>
2018-06-25 20:30:19 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,14 @@
 class SessionsController < ApplicationController
 
   def create
-    user = User.find_or_create_from_omniauth(auth_hash)
-    if user
+    user = User.find_by(email: auth_hash["info"]["email"])
+    if user.nil?
+      user = User.create do |u|
+        u.name = auth_hash["info"]["name"]
+        u.email = auth_hash["info"]["email"]
+        u.uid = auth_hash["uid"]
+      end
+    end
       session[:user_uid] = user.uid
     else
       redirect_to root_path
