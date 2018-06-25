a07a71d112f3e7f316ce2279d137edbd8d688402
Emily Jirles <emily.jirles@gmail.com>
2018-06-25 20:16:39 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   def find_or_create_from_omniauth(auth_hash)
-    user = User.find_or_create_by(email: auth_hash["info"]["email"])
+    user = User.find_by(email: auth_hash["info"]["email"])
     if user.nil?
       User.create do |u|
         u.name = auth_hash["info"]["name"]
